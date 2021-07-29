3d5515416b14a4b7578027153eeac55f7699f9a6
Ali-Said1 <asazizg1@gmail.com>
2021-07-29 16:00:49 +0200

    Fixed a bug, added responsive design

--- script.js
+++ script.js
@@ -9,13 +9,8 @@ function Your_Hobbie() {
   ResponseHobbie = prompt("Enter Your Hobbie: ", "Ex: Football");
 }
 function Your_Grade() {
-  ResponseGrade = prompt("Enter Your Grade: ", "Ex: Second Secondary");
+  ResponseGrade = prompt("Enter Your Grade: ", "");
 }
-
-var result = document.getElementById("result"),
-  resultText;
-
-
 
 
 const overlay = document.getElementById('overlay');
@@ -25,15 +20,14 @@ const popup  = document.getElementById('popup');
 function openpopup(){
   popup.classList.add("active");
   overlay.classList.add("active");
-  resultText = document.createTextNode(`  Name: ${ResponseName}
+
+    document.getElementById("result").innerHTML = `  Name: ${ResponseName}
   Age: ${ResponseAge}
   Hobbie: ${ResponseHobbie}
-  Grade: ${ResponseGrade}`);
-  result.appendChild(resultText);
+  Grade: ${ResponseGrade}`;
 }
 function closepopup(){
   popup.classList.remove("active");
   overlay.classList.remove("active");
-  resultText= null;
-  result.removeChild(resultText);
+  document.getElementById("result").innerHTML = ""
 }
